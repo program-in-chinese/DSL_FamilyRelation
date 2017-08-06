1062470123c5e02f49212a14d56640831fb56302
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-08-06 16:29:59 -0700

    通过单辈测试. 添加隔辈测试

--- src/com/github/programInChinese/模型/人物类.java
+++ src/com/github/programInChinese/模型/人物类.java
@@ -1,18 +1,22 @@
 package com.github.programInChinese.模型;
 
+import java.util.HashMap;
+
 public class 人物类 {
 
   private String 姓名;
+  
+  private HashMap<人物类, 关系类> 亲人 = new HashMap<>();
   
   public 人物类(String 姓名) {
     this.姓名 = 姓名;
   }
   
   public void 置家庭关系(人物类 他人, 关系类 关系) {
-    
+    亲人.put(他人, 关系);
   }
   
   public 关系类 取家庭关系(人物类 他人) {
-    return null;
+    return 亲人.get(他人);
   }
 }
