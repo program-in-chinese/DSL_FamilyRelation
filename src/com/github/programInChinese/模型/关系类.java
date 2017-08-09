d6469bcab16973bb53e5f3baf13ffbd8a10fef33
fromwheretowhere <fromwheretowhere.service@gmail.com>
2017-08-08 21:46:49 -0700

    拆分单元测试

--- src/com/github/programInChinese/模型/关系类.java
+++ src/com/github/programInChinese/模型/关系类.java
@@ -5,12 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 
 public enum 关系类 {
-  爸爸, 儿子, 爷爷, 哥哥;
+  爸爸, 儿子, 爷爷, 哥哥, 大伯;
 
   private static HashMap<List<关系类>, 关系类> 简化关系表 = new HashMap<>();
   
   static {
     简化关系表.put(Arrays.asList(关系类.爸爸, 关系类.爸爸), 关系类.爷爷);
+    简化关系表.put(Arrays.asList(关系类.爸爸, 关系类.哥哥), 关系类.大伯);
   }
   
   public static 关系类 简化关系(List<关系类> 关系链) {
---
+++ test/com/github/programInChinese/模型/关系测试类.java
@@ -0,0 +1,17 @@
+package com.github.programInChinese.模型;
+
+import static com.github.programInChinese.测试.断言.相等;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class 关系测试类 {
+
+  @Test
+  public void 简化关系() {
+    相等(关系类.爷爷, 关系类.简化关系(Arrays.asList(关系类.爸爸, 关系类.爸爸)));
+    相等(关系类.大伯, 关系类.简化关系(Arrays.asList(关系类.爸爸, 关系类.哥哥)));
+  }
+
+}
